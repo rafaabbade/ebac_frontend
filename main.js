70d5e1f733c5fe396d7fd55d973d9535645893ef
Rafael Abbade <rafaelvabbade@gmail.com>
2022-09-15 09:35:18 -0300

    commit projeto jquery1

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    
+
+
+    //outro evento com o JQuery, dessa vez com o button do tipo submit
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        //ao invés do .value, no jquery usamos a função .val
+        const novatarefa = $('#nova-tarefa').val();
+        const novoitem = $('<li class="clickable"></li>');
+        //antes do appendTo estamos criando uma tag com a imagem inserida e o o appendTo insere esse texto dentro de outra tag
+        $(novatarefa).appendTo(novoitem);
+
+        $(`
+        <li>
+            ${novatarefa}
+        </li>
+        `).appendTo(novoitem);
+        $(novoitem).appendTo('ul');
+        $('#nova-tarefa').val('');
+
+    })
+
+    //traçar uma linha sobre o item de uma lista
+    //usando um id na ul e organizando dessa forma para alterar apenas um li
+    $('#clickable').on('click', 'li', function() {
+        $(this).toggleClass('cortar')
+    });
+
+})
+
